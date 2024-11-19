128e2ad10644073c752aa22dfc201cbb825d9844
Diksha Sahare <sahare.d@northeastern.edu>
2024-11-19 16:18:54 -0500

    Adding Listing router file

--- api/service/routers/index.js
+++ api/service/routers/index.js
@@ -1,11 +1,13 @@
 import inquiryRouter from "./inquiry-router.js";
 import propertyRouter from "./property-router.js";
 import userRouter from "./user-router.js";
+import listingRouter from "./listings-router.js";
 
 const initializeRoutes = (app) => {
   app.use("/inquiry", inquiryRouter);
   app.use("/property", propertyRouter);
   app.use("/user", userRouter);
+  app.use("/listing", listingRouter);
 };
 
 export default initializeRoutes;
